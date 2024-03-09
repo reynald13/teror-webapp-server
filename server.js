2041124b64c18874a3737728bab3c19325112d11
Reynald Simanjuntak <reynaldsimanjuntak@MacBook-Air-Reynald.local>
2024-03-10 01:49:49 +0700

    added new fixes

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const config = require('./dabatase/db.js');
+const config = require('./database/db.js');
 const participantRouter = require('./routes/participants.js');
 
-mongoose.connect(config.DB)
+mongoose.connect(config.DB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Database is connected'))
-    .catch(err => console.error('Can not connect to the database', err));
+    .catch(err => {
+        console.error('Failed to connect to the database:', err);
+        process.exit(1); // Exit the process if database connection fails
+    });
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api', participantRouter);
 
-// Serve static files from the build folder
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Handle all other requests by serving the index.html file
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// Close the database connection properly when the app is closed or fails to start
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.log('Database connection closed due to app termination');
+        process.exit(0);
+    });
 });
 
-// Menggunakan nilai PORT yang tersedia atau port 4200 sebagai default
+// Using available PORT value or default port 4200
 const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
