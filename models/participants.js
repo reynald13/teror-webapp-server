8fbf03800fc0700423277744b30712c8e12b36c6
reynald13 <39654219+reynald13@users.noreply.github.com>
2024-03-18 13:29:28 +0700

    Update participants.js age data type

--- models/participants.js
+++ models/participants.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 
 const participantSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  phone: { type: String, required: true, unique: true, match: /^\d{10,12}$/ }, // Format nomor telepon harus 10 digit angka
+  phone: { type: String, required: true, unique: true, match: /^\d{10,12}$/ },
   city: { type: String, required: true },
-  age: { type: Number, required: true, min: 0 },
-  points: { type: Number, default: 0 }// Umur harus bilangan bulat positif
+  age: { type: String, required: true },
+  points: { type: Number, default: 0 }
 });
 
 // Menambahkan indeks pada bidang 'city' untuk pencarian lebih cepat
